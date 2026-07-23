0ee9dfa0cb85b203310890b95d4809e137562783
J <jtclarkjr@ucla.edu>
2026-07-23 15:51:51 +0900

    fix: load styles before SSR hydration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import type { StateTree } from 'pinia'
 
-import '@jtclarkjr/component-library-vue/style.css'
-import './assets/styles/main.scss'
-
 import App from './App.vue'
 import { apiClientKey, createApiClient, type ApiClient } from './api/client'
 import { createAppRouter } from './router'
